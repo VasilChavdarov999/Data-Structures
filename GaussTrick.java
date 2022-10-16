80e2a3c9ab95d8bb31191ecda9736ef889bfda0c
VasilChavdarov999 <62804466+VasilChavdarov999@users.noreply.github.com>
2022-10-16 10:27:55 +0300

    Add files via upload

--- GaussTrick.java
+++ GaussTrick.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -8,20 +7,18 @@ public class GaussTrick {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        int sum = 0;
 
-        for (int i = 0; i < numbers.size(); i++) {
-            int firstNum = numbers.get(i);
-            int lastNum = numbers.get(numbers.size() - 1);
-            sum = (firstNum+ lastNum);
-            System.out.print(sum + " ");
-
-            numbers.remove(firstNum);
-            numbers.remove(lastNum);
+        int halfSize = numbers.size()/2;
+        for (int i = 0; i < halfSize; i++) {
+            int sum = numbers.get(i) + numbers.get(numbers.size() - 1);
+            numbers.set(i, sum);
+            numbers.remove(numbers.size() - 1);
         }
-
+        for (Integer integer : numbers) {
+            System.out.print(integer + " ");
+        }
     }
 }
--- SumAdjacentEqualNumbers.java
+++ SumAdjacentEqualNumbers.java
@@ -1,5 +1,8 @@
-import java.lang.reflect.Array;
+
+
+
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,22 +12,27 @@ public class SumAdjacentEqualNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Double> numbers = Arrays.stream(scanner.nextLine().split(" "))
-                .map(Double::parseDouble)
-                .collect(Collectors.toList());
+
+        String nums = scanner.nextLine();
+        String[] split = nums.split("\\s+");
+
+        List<Double> numbers = new ArrayList<>();
+
+        for (int i = 0; i < split.length; i++) {
+            double num = Double.parseDouble(split[i]);
+            numbers.add(num);
+        }
 
         for (int i = 0; i < numbers.size() - 1; i++) {
             if (numbers.get(i).equals(numbers.get(i + 1))){
-                Double sum = numbers.get(i) + numbers.get(i + 1);
+                double sum = numbers.get(i) + numbers.get(i + 1);
+                numbers.remove(i);
                 numbers.set(i, sum);
-                numbers.remove(i + 1);
                 i = -1;
             }
         }
-
-        DecimalFormat decimalFormat = new DecimalFormat("0.#");
         for (Double number : numbers) {
-            System.out.print(decimalFormat.format(number) + " ");
+            System.out.print(new DecimalFormat("0.#").format(number) + " ");
         }
     }
 }
